234727b2c78c343eecc9514ea5f0e8291c75d781
SOFTWARE\achroscielewska <agnieszka.chroscielewska@altkom.pl>
2019-07-14 22:00:50 +0200

    book-list added

--- bookshelf-app/src/app/app.module.ts
+++ bookshelf-app/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from '@app/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from '@app/app.component';
 
 import { SHARED_ANGULAR_MATERIAL, HeaderComponent } from '@app/components';
@@ -15,7 +18,10 @@ import { BookEditorComponent, BookListComponent } from '@app/components/books';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule,
     SHARED_ANGULAR_MATERIAL
   ],
   providers: [],
--- bookshelf-app/src/app/components/books/book-list/book-list.component.ts
+++ bookshelf-app/src/app/components/books/book-list/book-list.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BookService } from '../book.service';
+import { Book } from '../Book';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
 
-  constructor() { }
+export class BookListComponent implements OnInit, OnDestroy {
+  panelOpenState = false;
+  books: Book[] = [];
+  isLoading = false;
+
+  private booksSub: Subscription;
+
+  constructor(public booksService: BookService) {}
 
   ngOnInit() {
+    this.isLoading = true;
+    this.booksService.getBooks();
+    this.booksSub = this.booksService.getBookUpdateListener().subscribe((data: Book[]) => {
+      this.books = data;
+      this.isLoading = false;
+    });
   }
 
+  onDelete(id: string) {
+    this.booksService.deleteBook(id);
+  }
+
+  ngOnDestroy() {
+    this.booksSub.unsubscribe();
+  }
 }
